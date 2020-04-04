f141a9439e1221404c389adf0434e5f024445b55
Jared Henry <jh75707@gmail.com>
2020-04-03 20:09:09 -0500

    idk

---
+++ cmd/pub/main.go
@@ -0,0 +1,64 @@
+// Copyright 2012-2019 The NATS Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"flag"
+	"github.com/nats-io/nats.go"
+	"log"
+	"os"
+	"time"
+)
+
+// NOTE: Can test with demo servers.
+// nats-pub -s demo.nats.io <subject> <msg>
+// nats-pub -s demo.nats.io:4443 <subject> <msg> (TLS version)
+
+func main() {
+	var urlss = os.Getenv("SERVERURL")
+	urls := &urlss
+	var reply = flag.String("reply", "", "Sets a specific reply subject")
+
+	// Connect Options.
+	opts := []nats.Option{nats.Name("NATS Sample Publisher")}
+
+	// Connect to NATS
+	nc, err := nats.Connect(*urls, opts...)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer nc.Close()
+
+	subj, msg := os.Getenv("SUBJECT"), []byte(time.Now().String())
+	t := time.NewTicker(5 * time.Second)
+
+	for {
+		select {
+		case <-t.C:
+			if reply != nil && *reply != "" {
+				nc.PublishRequest(string(subj), *reply, msg)
+			} else {
+				nc.Publish(string(subj), msg)
+			}
+
+			nc.Flush()
+
+			if err := nc.LastError(); err != nil {
+				log.Fatal(err)
+			} else {
+				log.Printf("Published [%s] : '%s'\n", subj, msg)
+			}
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/dualwave/nats
 
 go 1.13
+
+require (
+	github.com/nats-io/nats-server/v2 v2.1.6 // indirect
+	github.com/nats-io/nats.go v1.9.2
+)
